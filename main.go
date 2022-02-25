3b560621b8a8479ac15b6347ecbb053f6de95298
Jonathan Schmitt <jonathanschmitt.blog@gmail.com>
2022-02-25 09:49:18 -0300

    gitignore

--- main.go
+++ main.go
@@ -1,17 +1,11 @@
 package main
 
 import (
-	"flag"
-
 	"github.com/jonathanschmittblog/jsapi/routers"
 	"github.com/jonathanschmittblog/jsapi/servers"
 )
 
 func main() {
-	// Se estiver executando algum teste com "go test"
-	if flag.Lookup("test.v") != nil {
-		return
-	}
 	server, err := servers.New()
 	if err != nil {
 		println("Não foi possível iniciar o servidor. Verifique as variáveis de ambiente.")
